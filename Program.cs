a8cc94b38a9f63073f0b97d9f1a3a23c2a563292
Alex84 <fantom.rpg@mail.ru>
2023-11-23 13:34:52 +0300

    Исправил решение задачи

--- Program.cs
+++ Program.cs
@@ -8,6 +8,55 @@
 // [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
 // [“Russia”, “Denmark”, “Kazan”] → []
 
+// string[] CreateArray()
+// {
+// Console.Write("Введите размер массива: ");
+// int size = Convert.ToInt32(Console.ReadLine());
+// string[] array = new string[size];
+// for (int i = 0; i < size; i += 1)
+// {
+// Console.Write($"Введите {i + 1} элемент массива: ");
+// array[i] = Console.ReadLine();
+// }
+// return array;
+// }
+
+// void ShowArray(string[] array)
+// {
+// for (int i = 0; i < array.Length; i += 1)
+// Console.Write(array[i] + " ");
+// Console.WriteLine();
+// }
+
+// string[] ResultArray(string[] array)
+// {
+// int c = 0;
+// int j = 0;
+// for (int i = 0; i < array.Length; i += 1)
+// {
+// if (array[i].Length <= 3) c += 1;
+// }
+// string[] arrayRes = new string[c];
+// for (int i = 0; i < array.Length; i += 1)
+// {
+// if (array[i].Length <= 3)
+// {
+// arrayRes[j] = array[i];
+// j += 1;
+// }
+// }
+// return arrayRes;
+// }
+
+// string[] arrayFaсt = CreateArray();
+// Console.WriteLine();
+// Console.WriteLine("Созданный массив:");
+// ShowArray(arrayFaсt);
+// Console.WriteLine();
+// string[] arrayRes = ResultArray(arrayFaсt);
+// Console.WriteLine("Новый массив из строк, длина которых меньше, либо равна 3 символам:");
+// string result = "[" + string.Join(", ", arrayRes) + "]";
+// Console.WriteLine(result);
 string[] CreateArray()
 {
 Console.Write("Введите размер массива: ");
@@ -30,19 +79,19 @@ Console.WriteLine();
 
 string[] ResultArray(string[] array)
 {
-int c = 0;
-int j = 0;
+int a = 0;
+int b = 0;
 for (int i = 0; i < array.Length; i += 1)
 {
-if (array[i].Length <= 3) c += 1;
+if (array[i].Length <= 3) a += 1;
 }
-string[] arrayRes = new string[c];
+string[] arrayRes = new string[a];
 for (int i = 0; i < array.Length; i += 1)
 {
 if (array[i].Length <= 3)
 {
-arrayRes[j] = array[i];
-j += 1;
+arrayRes[b] = array[i];
+b += 1;
 }
 }
 return arrayRes;
